06288585f802847e65b1c98dc0364ed6b27a4cc0
Alan Wu <alan125999@gmail.com>
2021-06-15 16:47:11 +0800

    Finish basic test_alien test

--- test_alien.py
+++ test_alien.py
@@ -25,7 +25,17 @@ def test_check_edges(alien, x, result):
     assert (alien.check_edges() == True) == result
 
 @pytest.mark.parametrize("speed, direction, delta", [
-    (1.0, 1, 1),    # Positive speed + positive direction
+    (20.0,     1, 20),    # Positive int speed    + positive direction
+    (7.8,      1, 7),     # Positive float speed  + positive direction
+    (0.0,      1, 0),     # zero speed            + positive direction
+    (-3.0,     1, -3),    # Negtive int speed     + positive direction
+    (-5.2,     1, -6),    # Negtive float speed   + positive direction
+
+    (2.0,     -1, -2),    # Positive int speed    + negtive direction
+    (10.9,    -1, -11),   # Positive float speed  + negtive direction
+    (0.0,     -1, 0),     # zero speed            + negtive direction
+    (-33.0,   -1, 33),    # Negtive int speed     + negtive direction
+    (-80.795, -1, 80),    # Negtive float speed   + negtive direction
 ])
 def test_update(alien, speed, direction, delta):
     alien.settings.alien_speed = speed
